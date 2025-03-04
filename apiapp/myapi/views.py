1b058025c842dd17db4d5306ba08b37f11d2286d
shanmukhasaitejatadisetti <shanmukhtadisetti9@gmail.com>
2025-03-04 17:05:42 +0000

    updated files

--- apiapp/myapi/views.py
+++ apiapp/myapi/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-import random,math
+import random,math,requests
 
 @api_view(['GET'])
 def getquote(request):
@@ -12,16 +12,16 @@ def getquote(request):
     d={
         '0':'For every minute you are angry you lose sixty seconds of happiness. ― Ralph Waldo Emerson',
         '1':"I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best. ― Marilyn Monroe",
-        '2':'So many books, so little time. ― Frank Zappa {}'.format(r),
+        '2':'So many books, so little time. ― Frank Zappa',
         '3':"Two things are infinite: the universe and human stupidity; and I'm not sure about the universe. ― Albert Einstein",
         '4':"Be the change that you wish to see in the world. ― Mahatma Gandhi",
         '5':"If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals. ― J.K. Rowling, Harry Potter and the Goblet of Fire",
         '6':"If you tell the truth, you don't have to remember anything. ― Mark Twain",
         '7':"To live is the rarest thing in the world. Most people exist, that is all. ― Oscar Wilde",
-        '8':"A woman is like a tea bag; you never know how strong it is until it's in hot water. ― Eleanor Roosevelt",
+        '8':"A friend is someone who knows all about you and still loves you. ― Elbert Hubbard",
         '9':"I have not failed. I've just found 10,000 ways that won't work. ― Thomas A. Edison"
     }
-
+    
     result = {
         'Quote':d[str(r)]
     }
--- test.py
+++ test.py
@@ -1,6 +1,24 @@
 import requests
 
-url = "http://127.0.0.1:8000/quoteapi/"
-response = requests.get(url)
+# url = "http://127.0.0.1:8000/quoteapi/"
 
-print(response.json())
+# putitem
+
+url = "https://b3vajioieb.execute-api.us-east-1.amazonaws.com/kvpair/putitem/"
+
+# params =  {
+#             'key': 'quote1',
+#            'value': 'For every minute you are angry you lose sixty seconds of happiness. ― Ralph Waldo Emerson'
+#            }
+
+# response = requests.get(url, params=params)
+
+# print(response.json())
+
+# url = "https://b3vajioieb.execute-api.us-east-1.amazonaws.com/kvpair/getitem/"
+
+# params = {'key': 'quote1'}
+
+# response = requests.get(url, params=params)
+
+# print(response.json()['value'])
